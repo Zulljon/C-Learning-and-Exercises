2ee005a5bb1ad3ffe2cf7c4731ef073ba6286e79
zulljon <zulljondan@gmail.com>
2017-08-22 19:03:06 +0300

    kochan ch3 ex5

---
+++ kochan/chapter_3/ex_5/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdint.h>
+
+uint32_t main(uint32_t argc, uint8_t *argv[]){
+	uint32_t sum;
+/* COMPUTE RESULT */
+	sum = 25 + 37 - 19;
+/* DISPLAY RESULTS */
+	printf ("The answer is %i\n", sum);
+	return 0;
+}
