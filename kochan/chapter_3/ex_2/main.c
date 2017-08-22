ead5c5866d4e9cce17d73651de1d29989b34d1ce
zulljon <zulljondan@gmail.com>
2017-08-22 19:00:04 +0300

    kochan ch3 ex2

---
+++ kochan/chapter_3/ex_2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdint.h>
+
+uint32_t main(uint32_t argc, uint8_t *argv[]){
+	printf ("Testing...");
+	printf ("....1");
+	printf ("...2");
+	printf ("..3");
+	printf ("\n");
+	return 0;
+}
