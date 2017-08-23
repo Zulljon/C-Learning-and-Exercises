78e9bab3866ab5e9d3e50aac952300332e1396ae
zulljon <zulljondan@gmail.com>
2017-08-23 23:50:00 +0300

    kochan ch4 ex8 #fix
    math.h and modf() not needed

--- kochan/chapter_4/ex_8/main.c
+++ kochan/chapter_4/ex_8/main.c
@@ -1,24 +1,16 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <math.h>
-#include <float.h>
 
-double roundOFF(double i, double j){
-	double a,x,y,b;
-	x = i;
-	y = j;
-	b = modf(x,&y);
-	a = (i + j) - b ;
-	return a;
+uint32_t roundOFF(uint32_t i, uint32_t j){
+	uint32_t x = i;
+	uint32_t y = j;
+	return (i + j) - x%y;
 }
 
 uint32_t main(uint32_t argc, uint8_t *argv[]){
-	printf("round of 256 and 7 = %.5f\n", roundOFF(256.0f,7.0f) );
-	printf("round of 365 and 7 = %.5f\n", roundOFF(365.0f,7.0f) );
-	printf("round of 12.258,23 = %.5f\n", roundOFF(12.258,23.0f) );
-	printf("round of 996 and 4 = %.5f\n", roundOFF(996.0f,4.0f) );
-
-	double p = 7;
-	printf("256mod7 = %.30f\n", modf(256,&p) );
+	printf("round of 256 and 7 = %i\n", roundOFF(256,7) );
+	printf("round of 365 and 7 = %i\n", roundOFF(365,7) );
+	printf("round of 12258, 23 = %i\n", roundOFF(12258,23) );
+	printf("round of 996 and 4 = %i\n", roundOFF(996,4) );
 	return 0;
 }
