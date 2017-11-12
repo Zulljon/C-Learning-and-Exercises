f97e17cba791b39e1dc1ecb0c818c85157cb600a
zulljon <zulljondan@gmail.com>
2017-11-12 22:36:55 +0200

    kochan ch10 ex3

--- kochan/chapter_10/Program 10.7 Counting Words/main.c
+++ kochan/chapter_10/Program 10.7 Counting Words/main.c
@@ -14,8 +14,7 @@ int countWords (const char string[]){
 int i, wordCount = 0;
 bool lookingForWord = true;
 	for ( i = 0; string[i] != '\0'; ++i )
-	if ( alphabetic(string[i]) || string[i] == '`' || string[i] == '-'
-		|| string[i] == '.' || (string[i] >= '0' && string[i] <= '9') ){
+	if ( alphabetic(string[i]) ){
 		if ( lookingForWord ){
 			++wordCount;
 			lookingForWord = false;
@@ -29,11 +28,9 @@ bool lookingForWord = true;
 
 int main (void){
 const char text1[] = "Well, here goes.";
-const char text2[] = "And here we go... aga`in.";
-const char text3[] = "Look number 13.666";
+const char text2[] = "And here we go... again.";
 int countWords (const char string[]);
 printf ("%s - words = %i\n", text1, countWords (text1));
 printf ("%s - words = %i\n", text2, countWords (text2));
-printf ("%s - words = %i\n", text3, countWords (text3));
 return 0;
 }
--- kochan/chapter_10/Program 10.8 Counting Words in a Piece of Text/main.c
+++ kochan/chapter_10/Program 10.8 Counting Words in a Piece of Text/main.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 
 bool alphabetic (const char c){
-	if ( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') )
 		return true;
 	else
 		return false;
@@ -12,10 +12,9 @@ bool alphabetic (const char c){
 /* Function to count the number of words in a string */
 int countWords (const char string[]){
 int i, wordCount = 0;
-bool lookingForWord = true;
+bool lookingForWord = true, alphabetic (const char c);
 	for ( i = 0; string[i] != '\0'; ++i )
-	if ( alphabetic(string[i]) || string[i] == '`' || string[i] == '-'
-		|| string[i] == '.' || (string[i] >= '0' && string[i] <= '9') ){
+	if ( alphabetic(string[i]) ){
 		if ( lookingForWord ){
 			++wordCount;
 			lookingForWord = false;
@@ -29,11 +28,9 @@ bool lookingForWord = true;
 
 int main (void){
 const char text1[] = "Well, here goes.";
-const char text2[] = "And here we go... aga`in.";
-const char text3[] = "Look number 13.666";
+const char text2[] = "And here we go... again.";
 int countWords (const char string[]);
 printf ("%s - words = %i\n", text1, countWords (text1));
 printf ("%s - words = %i\n", text2, countWords (text2));
-printf ("%s - words = %i\n", text3, countWords (text3));
 return 0;
 }
